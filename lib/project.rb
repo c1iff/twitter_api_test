d62bf1975c95f2b4108572c2f85c5de0340aaa89
Chris Clifford <cclifford82@gmail.com>
2016-12-15 13:00:53 -0800

    add styling

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,10 @@
 class Project < ActiveRecord::Base
 
+  config = {
+  consumer_key:    "ueSBHjFSRvauM1Xdi86ZfG7dR",
+  consumer_secret: "bQEENdVrAAhNHPbqp2LFZUiy7M7JN7pGf2ND241V8HPp10QSv1",
+}
+
+$twitter = Twitter::REST::Client.new(config)
+
 end
